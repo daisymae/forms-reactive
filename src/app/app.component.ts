31e96b00a641bc6a65e0b3fb1a673ec83421372a
daisymae <daisymae@orcadiafarm.com>
2018-05-10 19:15:50 -0500

    Initialize form
    Set up the form controls for the form.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signupForm: FormGroup;
+
+  ngOnInit() {
+    // initialize before rendering template
+    this.signupForm = new FormGroup({
+      // first parameter is default value for the form field
+      'username': new FormControl(null),
+      'email': new FormControl(null),
+      'gender': new FormControl('male')
+    });
+  }
 }
